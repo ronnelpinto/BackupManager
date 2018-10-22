307b4a6227a139174533a7a6d9c94c0f0044aae7
Ronnel Maclean Pinto <ronnel_pinto@intuit.com>
2018-10-21 19:35:59 -0700

    Update makeperiodic.py

--- makeperiodic.py
+++ makeperiodic.py
@@ -4,8 +4,9 @@ os.system('''
   while true
   do
     python3 updatedb.py
-    sleep 10
+    sleep 20
   done
 ) &
 disown''')
 # run updatedb.py for every 10 secs to backup modified files
+# fixing wait issue by adding 20 sec wait instead of 10 sec.
