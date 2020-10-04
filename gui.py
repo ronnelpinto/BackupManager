bdccac2b8991d47ff921bbf0ab4b19a572230bf9
Ronnel Maclean Pinto <ronnel_pinto@intuit.com>
2020-10-04 21:58:17 +0800

    Adding tkinter details
    - Added details for Tkinter library to make it easier for contributors to make incremental changes
    - Fixed comments and made multiple code cleanups.

--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
+# The GUI for this project is built using Python 3 Tkinter library. You can read more about the same here - https://docs.python.org/3/library/tkinter.html
+
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox as box
 from backup import *
 from setupfordatabase import *
 
-
-
-class MainWIndow(Frame):        #the Main Frame class having instances of all the classes
+class MainWindow(Frame): # Main Frame class
     def __init__(self, parent):
         Frame.__init__(self, parent, background='#eed')
         self.parent = parent
@@ -21,52 +21,46 @@ class MainWIndow(Frame):        #the Main Frame class having instances of all th
         self.initInfoLabel()
         self.initScheduleButton()
         
-    def initUI(self):        #initialising the UI
+    def initUI(self):        # initialising the UI
         self.parent.title('Linux Backup Manager')
         self.pack(fill=BOTH, expand=1)
         sw = self.parent.winfo_screenwidth()
         sh = self.parent.winfo_screenheight()
         self.parent.geometry('500x500+%d+%d' % ((sw - 100)/2, (sh - 100)/2))
         
-    def initQuitButton(self):#initialises the Quit button
+    def initQuitButton(self):# initialises the Quit button
         quitButton = Button(self, text = "Quit", command = self.quit)
         quitButton.place(relx = 0.40, rely = 0.9, relwidth = 0.2)
 
-
-    def initScheduleButton(self):#initialises the Schedule button
+    def initScheduleButton(self):# initialises the Schedule button
         quitButton = Button(self, text = "Schedule to run\n backup periodically\n on working directory", command = self.invokeScheduler)
         quitButton.place(relx = 0.20, rely = 0.7, relwidth = 0.5)    
         
-    def initBackupButton(self):#initialises the Generate table button
+    def initBackupButton(self):# initialises the Generate table button
         genButton = Button(self, text = "Backup", command = self.invokebackup)
         genButton.place(relx = 0.10, rely = 0.9, relwidth = 0.2)
         
-        
-    def initInputFolder(self, textParam=''):  #text area to enter input folder
+    def initInputFolder(self, textParam=''):  # text area to enter input folder
         self.inputFolder = Text(self)
         self.inputFolder.place(relx = 0.05, rely = 0.15, relwidth = 0.6,relheight=0.08)
         self.inputFolder.insert(index = INSERT, chars = textParam)
 
-
-    def initExtension(self, textParam=''):  #text area to enter Extension type
+    def initExtension(self, textParam=''):  # text area to enter Extension type
         self.Extension = Text(self)
         self.Extension.place(relx = 0.7, rely = 0.15, relwidth = 0.2,relheight=0.08)
         self.Extension.insert(index = INSERT, chars = textParam)
 
-    def initTargetFolder(self, textParam=''):  #text area to enter target folder
+    def initTargetFolder(self, textParam=''):  # text area to enter target folder
         self.TargetFolder = Text(self)
         self.TargetFolder.place(relx = 0.05, rely = 0.5, relwidth = 0.6,relheight=0.08)
         self.TargetFolder.insert(index = INSERT, chars = textParam)
 
-
-    def initCompressionType(self, textParam=''):  #text area to enter Compression type
+    def initCompressionType(self, textParam=''):  # text area to enter Compression type
         self.CompressionType = Text(self)
         self.CompressionType.place(relx = 0.7, rely = 0.5, relwidth = 0.2,relheight=0.08)
         self.CompressionType.insert(index = INSERT, chars = textParam)
-
-
                 
-    def initLabels(self):   #labels for the various uneditable fields
+    def initLabels(self):   # labels for the various uneditable fields
         l1 = Label(self, text = "The folder to be backed up\n(leave blank if current directory)", background='#eed')
         l2 = Label(self, text = "The target folder to save backups\n(leave blank if default backup folder)", background='#eed')        
         l11 = Label(self, text = "Mention extension if you want \nto backup files with a \nparticular extension", background='#eed')
@@ -76,7 +70,7 @@ class MainWIndow(Frame):        #the Main Frame class having instances of all th
         l11.place(relx = 0.61, rely = 0.02, relwidth = 0.4)
         l22.place(relx = 0.61, rely = 0.4, relwidth = 0.4)
                 
-    def initInfoLabel(self, textParam = "Information: ", flag=1):   #info label to check for success or failure
+    def initInfoLabel(self, textParam = "Information: ", flag=1):   # info label to check for success or failure
         if flag is 1:
             l3 = Label(self, text = textParam, background='#3f3')
         else:
@@ -84,8 +78,7 @@ class MainWIndow(Frame):        #the Main Frame class having instances of all th
         l3.place(relx = 0.79, rely = 0.70, relwidth = 0.2)    
         
     def invokebackup(self):
-        #call to initialise code module of backup.py file
-        check=len(self.Extension.get('0.0',END).strip("\n"))
+        check=len(self.Extension.get('0.0',END).strip("\n"))  # call to initialise code module of backup.py file
         if check==0:
         	backup = initialise(str(self.inputFolder.get( '0.0', END)),'no',str(self.Extension.get( '0.0', END)),'all',str(self.TargetFolder.get( '0.0', END)),str(self.CompressionType.get( '0.0', END)))
         else: 
@@ -98,15 +91,14 @@ class MainWIndow(Frame):        #the Main Frame class having instances of all th
             infoString = 'Error.\nThe files\n have not\nbacked up' 
             self.initInfoLabel(infoString, flag = 0)
 
-    def invokeScheduler(self):  #code to call the Scheduler to schedule the backup of a particular working folder
+    def invokeScheduler(self):  # invoke scheduler for the backup
         a=os.getcwd()
         acceptDir()
         createDB()
         os.chdir(a)
         os.system("python3 makeperiodic.py")
-	
-	
-def main():     #main method to define the Tkinter interface
+		
+def main():     # main method to define the Tkinter interface
     root = Tk()
-    app = MainWIndow(root)
+    app = MainWindow(root)
     root.mainloop()
